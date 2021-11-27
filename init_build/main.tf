6ba0810c486151b77637f3eb338e236a8cb74f11
Stephane REYTAN <srnfr@users.noreply.github.com>
2021-11-27 08:37:30 +0100

    Update main.tf

--- init_build/main.tf
+++ init_build/main.tf
@@ -1,9 +1,50 @@
-# main.tf
-# Configure the DigitalOcean Provider
-provider "digitalocean" {
-  version = "~> 1.22"
-  # token   = var.do_token # This is the DO API token.
-  # Alternatively, this can also be specified using environment variables ordered by precedence:
-  #   DIGITALOCEAN_TOKEN, 
-  #   DIGITALOCEAN_ACCESS_TOKEN
+variable "region_name" {
+  type        = string
+}
+variable "domain_name" {
+  type        = string
+}
+variable "droplet_size" {
+  type        = string
+}
+variable "droplet_image" {
+  type        = string
+}
+variable "prefix" {
+  type        = string
+}
+
+variable "tag_name" {
+  type        = string
+}
+
+variable "ssh_keys" {
+  default = []
+}
+
+resource "digitalocean_tag" "tag" {
+  name = var.tag_name
+##  lifecycle {
+##    prevent_destroy = true
+##  }
+}
+
+resource "digitalocean_droplet" "docker" {
+  image  = var.droplet_image
+  name   = "${var.prefix}-docker"
+  region = var.region_name
+  size   = var.droplet_size
+  tags               = [digitalocean_tag.tag.id]
+  monitoring         = "true"
+ ## private_networking = "true"
+  ssh_keys           = var.ssh_keys
+  user_data = "${file("cloud-init.yaml")}"
+##  user_data = "#cloud-config\nruncmd:\n  - /usr/bin/docker run -d -p 80:80 sagikazarmark/dvwa"
+}
+
+resource "digitalocean_record" "docker" {
+  domain = var.domain_name
+  type   = "A"
+  name   = "docker"
+  value  = digitalocean_droplet.docker.ipv4_address
 }
