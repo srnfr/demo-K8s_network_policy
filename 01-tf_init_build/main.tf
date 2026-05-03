fc722f70466fc269c6628d2f0f5c1dc3b374d86a
Stephane <srnfr@users.noreply.github.com>
2026-05-03 22:16:30 +0200

    Update IP ranges for VPC and subnets

--- 01-tf_init_build/main.tf
+++ 01-tf_init_build/main.tf
@@ -30,7 +30,7 @@ resource "digitalocean_vpc" "vpc" {
   region   = var.region_name
 
   # Chaque cluster a sa propre plage /20 : 10.150.0.0/20, 10.151.0.0/20, etc.
-  ip_range = cidrsubnet("10.150.0.0/8", 8, count.index)  # 10.150.0.0/16, 10.151.0.0/16...
+  ip_range = cidrsubnet("10.170.0.0/16", 8, count.index)  # 10.150.0.0/16, 10.151.0.0/16...
 
 }
 
@@ -52,8 +52,8 @@ resource "digitalocean_kubernetes_cluster" "cluster" {
   vpc_uuid = digitalocean_vpc.vpc[count.index].id
 
   # Subnets pods et services distincts par cluster
-  cluster_subnet = cidrsubnet("10.160.0.0/8", 8, count.index)  # 10.160.0.0/16, 10.161.0.0/16...
-  service_subnet = cidrsubnet("10.200.0.0/8", 8, count.index)  # 10.200.0.0/16, 10.201.0.0/16...
+  cluster_subnet = cidrsubnet("10.180.0.0/16", 8, count.index)  # 10.160.0.0/16, 10.161.0.0/16...
+  service_subnet = cidrsubnet("10.190.0.0/16", 8, count.index)  # 10.200.0.0/16, 10.201.0.0/16...
 
 }
 
